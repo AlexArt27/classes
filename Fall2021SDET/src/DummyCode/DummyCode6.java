039ad5bdec4187cf5a16fc47f245fbc3ec77c58c
AlexArt27 <alex.artamoshkin@gmail.com>
2022-02-07 18:43:06 -0600

    home work

--- Fall2021SDET/src/DummyCode/DummyCode6.java
+++ Fall2021SDET/src/DummyCode/DummyCode6.java
@@ -1,9 +1,22 @@
 package DummyCode;
 
+import java.util.Arrays;
+
 public class DummyCode6 {
     public static void main(String[] args) {
-        String str = "Alex";
+        String str = "sdel";
 
-        System.out.println(str.substring(0,3));
+//        System.out.println((str.substring(0,1)+str.substring(4,str.length())));
+        System.out.println(str.substring(1,3));
+//        System.out.println(str.substring(1,4).equals("del"));
+
+        if (str.length()>=4){
+        if(str.substring(1,4).equals("del")){
+            System.out.println((str.substring(0,1)+str.substring(4,str.length())));
+        }else{
+            System.out.println(str);
+        }
+        }
+        System.out.println(str);
     }
 }
